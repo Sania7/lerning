f7148c4acee7d758c2e46c94e279bbb23e71e5c8
macbook <79824177721@yandex.ru>
2020-10-23 11:11:08 +0500

    Конструкция try-catch.

--- src/exeption/JdbcConfig.java
+++ src/exeption/JdbcConfig.java
@@ -6,6 +6,16 @@ package exeption;
  * какие прерывания могут случиться с этим методом.
  * Через запятую можно указать несколько исключений.
  * В этом случае мы передаем ответственность за исключение коду, который будет использовать наш метод.
+ *
+ * Конструкция try-catch.
+ * Эта конструкция позволяет выполнить методы, которые могут прерваться с исключением и обработать эти исключения.
+ * Мы убрали из сигнатуры объявление throws и обернули метод load в конструкцию try-catch.
+ * Блок try содержит операторы, которые мы хотим выполнить.
+ * Блок catch содержит объявление исключение. Если операторы в блоке try вызовут исключение UserInputException,
+ * то программа перейдет к выполнению блока catch.
+ * В блоке catch написан только один оператор.
+ * e.printStackTrace();
+ * Этот оператор выведет на консоль стек вызовов методов от начала программы до оператора, который вызвал прерывания.
  */
 public class JdbcConfig {
     public static void load(String url) throws UserInputException {
@@ -14,7 +24,11 @@ public class JdbcConfig {
         }
     }
 
-    public static void main(String[] args) throws UserInputException {
-        load("jdbc://localhost:8080");
+    public static void main(String[] args) {
+        try {
+            load("jdbc://localhost:8080");
+        } catch (UserInputException e) {
+            e.printStackTrace();
+        }
     }
 }
