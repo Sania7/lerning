b0640a2a3e7d0dd674f50fdf689f99c8cf747373
macbook <79824177721@yandex.ru>
2020-10-23 09:56:15 +0500

    NullPointerException

--- src/exeption/FindMaxLength.java
+++ src/exeption/FindMaxLength.java
@@ -2,6 +2,7 @@ package exeption;
 
 /**
  * 0.1. java.lang.NullPointerException
+ * Сделать проверку чтобы не падало исключение
  */
 public class FindMaxLength {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class FindMaxLength {
         int max = 0;
         for (int index = 0; index < shops.length; index++) {
             String el = shops[index];
-            if (el != null && el.length() > max) {
+            if (el != null && el.length() > max) { // проверка
                 max = el.length();
             }
         }
