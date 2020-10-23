677e6625993e0bc88e58b7253df64c2840ee9dc8
macbook <79824177721@yandex.ru>
2020-10-23 10:04:52 +0500

    0.2. Кидаем исключение - throw new RuntimeException

---
+++ src/exeption/Fact.java
@@ -0,0 +1,23 @@
+package exeption;
+
+/**
+ * 0.2. Кидаем исключение - throw new RuntimeException
+ * java.lang.IllegalArgumentException - метод вызывает с не корректными параметрами.
+ * java.lang.IllegalStateException - метод вызывается с объекта в не корректном состоянии.
+ * java.lang.NullPointerException - методы вызывается у переменной, которая инициализирована null ссылкой.
+ */
+
+public class Fact {
+    public static void main(String[] args) {
+    }
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N must not be less than 0.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
